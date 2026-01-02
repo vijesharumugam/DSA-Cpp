55c536ca1ef51b992ff34b139af69cf9f83f6e37
vijesharumugam <vijeshvijesh2601@gmail.com>
2026-01-02 14:36:29 +0530

    LinkedList oddevenLL

---
+++ LinkedList/oddevenLL.cpp
@@ -0,0 +1,23 @@
+//  328 Odd Even Linked List (Leetcode)
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+        return head;
+    }
+};
